fdc60d916b4c30ba05ab540837e09e48d98df482
magotts <techybet@gmail.com>
2022-01-04 22:57:38 -0500

    cange denver to markham

--- script.js
+++ script.js
@@ -44,3 +44,12 @@ document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
 
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Markham");
